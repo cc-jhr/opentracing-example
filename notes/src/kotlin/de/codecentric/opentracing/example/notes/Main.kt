f661151a1ab568784a3983caae20f5696398af6e
cc-jhr <34243889+cc-jhr@users.noreply.github.com>
2018-01-04 10:48:32 +0100

    Placed all endpoints under the same path

--- notes/src/kotlin/de/codecentric/opentracing/example/notes/Main.kt
+++ notes/src/kotlin/de/codecentric/opentracing/example/notes/Main.kt
@@ -1,34 +1,38 @@
 package de.codecentric.opentracing.example.notes
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import spark.Spark.path
 import spark.kotlin.*
-
 
 
 fun main(args: Array<String>) {
     val objectMapper = ObjectMapper()
     NoteInMemoryDb.addNote(Note(null, "Initial note"))
 
+
     ignite().apply {
         port(8082)
-        get("/notes") {
-            response.type("application/json")
-            objectMapper.writeValueAsString(NoteInMemoryDb.fetchAll().values)
-        }
 
-        post("/notes") {
-            val body: String = request.body()
-            val note: Note = objectMapper.readValue(body, Note::class.java)
-            NoteInMemoryDb.addNote(note)
-        }
+        path("/notes", {
+            get("/") {
+                response.type("application/json")
+                objectMapper.writeValueAsString(NoteInMemoryDb.fetchAll().values)
+            }
 
-        get("/notes/:id") {
-            response.type("application/json")
-            objectMapper.writeValueAsString(NoteInMemoryDb.fetchNote(request.params(":id").toInt()))
-        }
+            post("/") {
+                val body: String = request.body()
+                val note: Note = objectMapper.readValue(body, Note::class.java)
+                NoteInMemoryDb.addNote(note)
+            }
 
-        delete("/notes/:id") {
-            NoteInMemoryDb.deleteNote(request.params(":id").toInt())
-        }
+            get("/:id") {
+                response.type("application/json")
+                objectMapper.writeValueAsString(NoteInMemoryDb.fetchNote(request.params(":id").toInt()))
+            }
+
+            delete("/:id") {
+                NoteInMemoryDb.deleteNote(request.params(":id").toInt())
+            }
+        })
     }
 }
--- reminder/src/kotlin/de/codecentric/opentracing/example/reminder/Main.kt
+++ reminder/src/kotlin/de/codecentric/opentracing/example/reminder/Main.kt
@@ -3,6 +3,7 @@ package de.codecentric.opentracing.example.reminder
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import spark.Spark.path
 import spark.kotlin.*
 import java.time.LocalDateTime
 
@@ -13,29 +14,32 @@ fun main(args: Array<String>) {
         registerModule(JavaTimeModule())
         configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
     }
+
     ReminderInMemoryDb.addReminder(Reminder(null, "Initial reminder", LocalDateTime.now()))
 
     ignite().apply {
         port(8081)
 
-        get("/reminder") {
-            response.type("application/json")
-            objectMapper.writeValueAsString(ReminderInMemoryDb.fetchAll().values)
-        }
+        path("/reminder", {
+            get("/") {
+                response.type("application/json")
+                objectMapper.writeValueAsString(ReminderInMemoryDb.fetchAll().values)
+            }
 
-        post("/reminder") {
-            val body: String = request.body()
-            val reminder: Reminder = objectMapper.readValue(body, Reminder::class.java)
-            ReminderInMemoryDb.addReminder(reminder)
-        }
+            post("/") {
+                val body: String = request.body()
+                val reminder: Reminder = objectMapper.readValue(body, Reminder::class.java)
+                ReminderInMemoryDb.addReminder(reminder)
+            }
 
-        get("/reminder/:id") {
-            response.type("application/json")
-            objectMapper.writeValueAsString(ReminderInMemoryDb.fetchReminder(request.params(":id").toInt()))
-        }
+            get("/:id") {
+                response.type("application/json")
+                objectMapper.writeValueAsString(ReminderInMemoryDb.fetchReminder(request.params(":id").toInt()))
+            }
 
-        delete("/reminder/:id") {
-            ReminderInMemoryDb.deleteReminder(request.params(":id").toInt())
-        }
+            delete("/:id") {
+                ReminderInMemoryDb.deleteReminder(request.params(":id").toInt())
+            }
+        })
     }
 }
